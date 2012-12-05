d26f34804ffb0e278c2577fea07dbe8a467dbbde
Jeff <jrschell@ucsd.edu>
2012-12-05 01:14:06 -0800

    Added Basic export to Excel Functionality.

--- web/routes/admin/admin.php
+++ web/routes/admin/admin.php
@@ -3,6 +3,7 @@
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session;
+use \PHPExcel;
 
 /**
  * All URLS will be prefixed with /admin/ automatically
@@ -123,6 +124,173 @@ $admin->get('/logout/', function() use ($app){
 	return $app->redirect($app['url_generator']->generate('admin_login'));
 
 })->bind('admin_logout');
+
+/*
+
+	TEMPORARY EXPORT FEATURE FOR VISUALIZING THE DATA THROUGH
+	EXCEL, THIS SHOULD BE REPLACED TO HAVE THE ADMIN PANEL
+	DO ALL THE FANCY THINGS.
+
+ */
+$admin->get('/export/', function() use($app){
+
+	/* List datasheets for export */
+	$locations = $app['paris']->getModel('Coastkeeper\Location')->find_many();
+
+	return $app['twig']->render('admin/export/list.twig.html', array(
+		"locations" => $locations
+	));
+
+})->before($admin_login_check)->bind('admin_export');
+
+$admin->get('/export/{id}/', function($id) use ($app){
+
+	/* Set up the Top row for the sheet. */
+	$sheet_header = array();
+	$sheet_header["A"] = "Date";
+	$sheet_header["B"] = "Volunteer 1";
+	$sheet_header["C"] = "Volunteer 2";
+	$sheet_header["D"] = "Start Time";
+	$sheet_header["E"] = "End Time";
+	$sheet_header_prefix = '';
+	$sheet_header_start_pos = ord('F');
+	$sheet_header_start_index = 0;
+
+	/* This will hold where the Entry is in the "Sheet_header" array */
+	$entries_position = array();
+
+	/* Get the location */
+	$location = $app['paris']->getModel('Coastkeeper\Location')->find_one($id);
+
+	$filename = $location->name . '-' . date('Y-m-d');
+
+	/* Get the datasheet, we'll need to set the header up */
+	$datasheet = $location->datasheet()->find_one();
+
+	/* We're going to need to all the Entries for the datasheet as well. */
+	$datasheet_categories = $datasheet->categories()->find_many();
+	/* Step through the categories to get the entires. */
+	foreach($datasheet_categories as $category)
+	{
+		$entries = $category->entries()->find_many();
+
+		/* Step through each entry and add them to the entries array. */
+		foreach($entries as $entry){
+			/* Get the next column */
+			$column = $sheet_header_prefix . chr($sheet_header_start_pos + $sheet_header_start_index);
+			/* Add it to the sheet header */
+			$sheet_header[$column] = $entry->name;
+			/* Add this position to the entries_position array */
+			$entries_position[$entry->id] = $column;
+			/* Increase the index */
+			$sheet_header_start_index++;
+			/* If we've reached Z, we need to start doing Prefixes. */
+			if(($sheet_header_start_pos + $sheet_header_start_index) >= ord('Z'))
+			{
+				if($sheet_header_prefix == ''){
+					$sheet_header_prefix = 'A';
+				}else{
+					$sheet_header_prefix = ord($sheet_header_prefix);
+					$sheet_header_prefix++;
+					$sheet_header_prefix = chr($sheet_header_prefix);
+				}
+				$sheet_header_start_pos = ord('A');
+				$sheet_header_start_index = 0;
+			}
+		}
+
+	}
+
+	/* 
+		We now have our header row that we can use to'
+		populate an excel sheet.
+	*/
+
+	/* Create a new excel object. */
+	$excel = new PHPExcel();
+
+	/*
+		Lets get all the sections of the location.
+	*/
+	$sections = $location->sections()->find_many();
+
+	/* For each section, lets make a sheet */
+	foreach($sections as $section)
+	{
+		/* Set the current sheet */
+		$sheet = $excel->createSheet();
+
+		/* Set the worksheet title */
+		$sheet->setTitle(substr(preg_replace("/[^A-Za-z0-9]/","",$section->name), 0, 31));
+
+		/* The current row we're working with */
+		$row_index = 1;
+
+		/* Fill in our headers. */
+		foreach($sheet_header as $key => $value)
+		{
+			$sheet->setCellValue($key . $row_index, $value);
+		}
+
+		$row_index++;
+
+		/* Get all of the PatrolEntries for this section */
+		$patrol_entries = $section->patrol_entry()->find_many();
+
+		/* For each patrol entry, fill out a row */
+		foreach($patrol_entries as $patrol_entry)
+		{
+			/* Get the parent patrol. */
+			$patrol = $patrol_entry->patrol()->find_one();
+
+			/* Set the date */
+			$sheet->setCellValue('A' . $row_index, $patrol->date);
+
+			/* Get Volunteer information */
+			$volunteer = $patrol->volunteer()->find_one();
+
+			/* Set the values */
+			$sheet->setCellValue('B' . $row_index, $volunteer->first_name . ' ' . $volunteer->last_name);
+			if($patrol->coastkeeper_partner_id){
+				$partner = $app['paris']->getModel('Coastkeeper\Volunteer')
+									->find_one($patrol->coastkeeper_partner_id);
+				$sheet->setCellValue('C' . $row_index, $partner->first_name . ' ' . $partner->last_name);
+			}
+
+			/* Set the start time and end time */
+			$sheet->setCellValue('D' . $row_index , $patrol_entry->start_time);
+			$sheet->setCellValue('E' . $row_index , $patrol_entry->end_time);
+
+			/* Now get all the tallies for this patrol... */
+			$tallies = $patrol_entry->patrol_tallies()->find_many();
+
+			foreach($tallies as $tally)
+			{
+				$sheet->setCellValue($entries_position[$tally->coastkeeper_datasheet_entry_id] . $row_index, $tally->tally);
+			}
+
+			/* increase the row */
+			$row_index++;
+
+		}
+	}
+
+	/* Remove the cover Sheet */
+	$excel->removeSheetByIndex(0);
+
+	/* Set the header values. */
+	header("Content-Type: application/vnd.ms-excel");
+	header("Content-Disposition: attachment; filename=\"$filename.xls\"");
+	header("Cache-control: max-age=0");
+
+	/* Write the information */
+	$writer = PHPExcel_IOFactory::createWriter($excel, "Excel5");
+	$writer->save("php://output");
+
+	/* exit, as we don't need silex to do anything */
+	exit;
+
+})->assert('id','\d+')->before($admin_login_check)->bind('admin_export_data');
 
 
 /*
