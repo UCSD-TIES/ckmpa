fa09fe894f43dc2864f1ec8a97f9d3a06a834eb3
David Drabik <djdrabik@gmail.com>
2013-02-04 12:30:09 -0800

    updated locations route to point at new locations view

--- web/routes/admin/locations.php
+++ web/routes/admin/locations.php
@@ -22,7 +22,7 @@ $routes->get('/', function() use ($app){
         $locations = $app['paris']->getModel('Coastkeeper\Location')->find_many();
 
         /* Display the list of locations */
-        return $app['twig']->render('admin/export/list.twig.html', array(
+        return $app['twig']->render('admin/locations/list.twig.html', array(
             'locations' => $locations
         ));
         
@@ -109,154 +109,11 @@ $routes->match('/create/', function(Request $request) use ($app){
 	}
 
 	/* Render the create form. */
-	return $app['twig']->render('admin/volunteers/create.twig.html', array(
+	return $app['twig']->render('admin/locations/create.twig.html', array(
 		"errors" => $errors,
 	));
 
 })->before($admin_login_check)->bind('admin_locations_create');
 
-$routes->get('/{id}/', function($id) use ($app){
-
-	/* Find the volunteer and send it to the template. */
-	$volunteer = $app['paris']->getModel('Coastkeeper\Volunteer')->find_one($id);
-
-	/* Get information on a certain volunteer */
-	return $app['twig']->render('admin/volunteers/view.twig.html', array(
-		"volunteer" => $volunteer
-	));
-
-})->assert('id','\d+')
-  ->before($admin_login_check)
-  ->bind('admin_volunteer_view');
-
-$routes->match('/{id}/edit/', function(Request $request, $id) use ($app){
-
-	/* Array for errors */
-	$errors = array();
-
-	/* Find the volunteer */
-	$volunteer = $app['paris']->getModel('Coastkeeper\Volunteer')
-		->find_one($id);
-
-	if('POST' == $request->getMethod() && $volunteer)
-	{
-
-		$first_name = $request->get('first_name');
-		$last_name = $request->get('last_name');
-		$username = $request->get('username');
-		$password = $request->get('new_password');
-		$passwordb = $request->get('new_password_b');
-		$is_admin = $request->get('is_admin');
-
-		
-		/*
-			If they didn't enter a first_name..
-		 */
-		if(empty($first_name) || !ctype_alpha($first_name))
-		{
-			$errors['first_name'] = "First name can only contain letters.";
-		}
-
-		/*
-			If they didn't enter a last_name..
-		 */
-		if(empty($last_name) || !ctype_alpha($last_name))
-		{
-			$errors['last_name'] = "Last name can only contain letters.";
-		}
-
-		/*
-			If they didn't enter a username, or if
-			the username isn't alphanumberic
-			[A-Z,a-z,0-9]
-		 */
-		if(empty($username) || !ctype_alnum($username))
-		{
-			$errors['username'] = "A username can only contain letters and numbers.";
-		}
-
-		/* 
-			If we're changing the password, make sure
-			that the password fields match.
-			This is to make sure the password was typed
-			correctly.
-			A password must be at least 5 characters.
-		 */
-		if(!empty($password))
-		{
-			if($password != $passwordb){
-				$errors['new_password'] = "Passwords entered do not match.";
-			}
-
-			if(strlen($password) < 5)
-			{
-				$errors['new_password'] = "Passwords must be at least 5 characters.";
-			}
-		}
-
-		/* If is_admin isn't set, or is not a valid number */
-		if(empty($is_admin) || !is_numeric($is_admin) || ($is_admin > 1))
-		{
-			$is_admin = 0;
-		}
-
-		/*
-			If there are no errors, update the volunteer.
-		 */
-		if(count($errors) <= 0){
-			$volunteer->first_name = $first_name;
-			$volunteer->last_name = $last_name;
-			$volunteer->username = $username;
-			if(!empty($password))
-			{
-				$volunteer->password = md5($password);
-			}
-			$volunteer->is_admin = $is_admin;
-
-			/* Save the new information */
-			$volunteer->save();
-
-			/* Redirect to volunteer page */
-			return $app->redirect(
-				$app['url_generator']->generate('admin_volunteer_view', array("id" => $id))
-			);
-		}
-	}
-
-	/* Render the edit volunteer form. */
-	return $app['twig']->render('admin/volunteers/edit.twig.html', array(
-		'volunteer' => $volunteer,
-		'errors' => $errors
-	));
-
-})->assert('id','\d+')
-  ->before($admin_login_check)
-  ->bind('admin_volunteer_edit');
-
-$routes->match('/{id}/delete/', function(Request $request, $id) use ($app){
-
-	/* Get the volunteer */
-	$volunteer = $app['paris']->getModel('Coastkeeper\Volunteer')->find_one($id);
-
-	if('POST' == $request->getMethod() && $volunteer)
-	{
-		/* If the delete was approved. */
-		if($request->get('approve_delete'))
-		{
-			/* Delete the volunteer and return to the volunteer list. */
-			$volunteer->delete();
-
-			return $app->redirect($app['url_generator']->generate('admin_volunteers'));
-		}
-	}
-
-	/* display "are you sure" form. */
-	return $app['twig']->render('admin/volunteers/delete.twig.html', array(
-		"volunteer" => $volunteer
-	));
-
-})->assert('id','\d+')
-  ->before($admin_login_check)
-  ->bind('admin_volunteer_delete');
 
 return $routes;
