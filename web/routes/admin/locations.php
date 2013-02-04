ded613180eecf22df16f4a182698de4fe56def1c
David Drabik <djdrabik@gmail.com>
2013-02-04 13:44:15 -0800

    Revert "added create location logic"
    This reverts commit a37ce428537c664fda54982cb6875600feabd030.

--- web/routes/admin/locations.php
+++ web/routes/admin/locations.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session;
 
 /*
- *  All URLS will be prefixed with /admin/locations/ automatically
+ *  All URLS will be prefixed with /admin/volunteers/ automatically
  *  by admin.php
  */
 
@@ -37,41 +37,73 @@ $routes->match('/create/', function(Request $request) use ($app){
 	if('POST' == $request->getMethod())
 	{
 
-		$location_name = $request->get('location_name');
+		$first_name = $request->get('first_name');
+		$last_name = $request->get('last_name');
+		$username = $request->get('username');
+		$is_admin = $request->get('is_admin');
+		$password = $request->get('new_password');
+		$passwordb = $request->get('new_password_b');
 
 		/* Validity Checks. */
 
-                /* Location name cannot be blank */
-		if( empty($location_name) )
+		/* First name must be letters and not blank. */
+		if(empty($first_name) || !ctype_alpha($first_name))
 		{
-			$errors['location_name'] = "Please enter an name for the new location";
+			$errors['first_name'] = "First name can only contain letters.";
 		}
 
-                /* Name must consist of letters and numbers */
-                /*if( !ctype_alnum($location_name) ) {
-                  $errors['location_name'] = "Please use only letters and/or numbers for the location's name";
-                }*/
+		/* Last name must be letters and not blank. */
+		if(empty($last_name) || !ctype_alpha($last_name))
+		{
+			$errors['last_name'] = "Last name can only contain letters.";
+		}
 
-                /* Name must be unique */
-                if( $app['paris']->getModel('Coastkeeper\Location')
-                                              ->where_equal('name', $location_name)
-                                              ->find_one()) {
-                  $errors['location_name'] = "There is already a location with that name";
-                }
+		/* Username must be alphanumeric */
+		if(empty($username) || !ctype_alnum($username)){
+			$errors['username'] = "Username must contain only letters or numbers.";
+		}
 
+		/* Passwords much match, not be blank, and at least 5 characters. */
+		if(empty($password))
+		{
+			$errors['new_password'] = "Password cannot be blank.";
+		}else if(strlen($password) < 5){
+			$errors['new_password'] = "Password must be at least 5 characters.";
+		}else if($password != $passwordb){
+			$errors['new_password'] = "Passwords must match.";
+		}
 
+		/* check and fill "is_admin" */
+		if(empty($is_admin) || !is_numeric($admin) || ($admin > 1))
+		{
+			$is_admin = 0;
+		}
 
-		/* If everything is ok, create the new location */
+		/* Username must be unique */
+		if($app['paris']->getModel('Coastkeeper\Volunteer')
+						->where_equal('username', $username)
+						->find_one())
+		{
+
+			$errors['username'] = "Username already in use.";
+
+		}
+
+		/* If everything is ok, create the new user*/
 		if(count($errors) <= 0)
 		{
-			$location = $app['paris']->getModel('Coastkeeper\Location')->create();
-			$location->name = $location_name;
+			$volunteer = $app['paris']->getModel('Coastkeeper\Volunteer')->create();
+			$volunteer->first_name = $first_name;
+			$volunteer->last_name = $last_name;
+			$volunteer->username = $username;
+			$volunteer->password = md5($password);
+			$volunteer->is_admin = $is_admin;
 
-			$location->save();
+			$volunteer->save();
 
-                        $locations = $app['paris']->getModel('Coastkeeper\Location')->find_many();
-
-			return $app->redirect($app['url_generator']->generate('admin_locations'));
+			return $app->redirect($app['url_generator']->generate('admin_volunteer_view', array(
+				"id"=>$volunteer->id
+			)));
 		}
 
 	}
