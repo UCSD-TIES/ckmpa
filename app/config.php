d332bb6c75906dfc9601aa0f68e911f46febdc04
David Drabik <djdrabik@gmail.com>
2013-02-13 17:55:19 -0800

    Created config file

---
+++ app/config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Configure Timezone
+ */
+date_default_timezone_set('America/Los_Angeles');
+
+
+/*
+ * APP_PATH
+ */
+define('APP_PATH', __DIR__ . '/../web');
+
+/* 
+ * Database Configuration Options for MySQL
+ */
+define('DBHOST', 'localhost');
+define('DBNAME', 'coastkeeper');
+define('DBUSER', 'root');
+define('DBPASS', '');
