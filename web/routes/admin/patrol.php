53bf1db7909337cea85ec5cebdc73692b0dd4608
Shannon Harris <svjharris@gmail.com>
2013-03-07 12:07:13 -0800

    Patrol page now displays tally data

--- web/routes/admin/patrol.php
+++ web/routes/admin/patrol.php
@@ -6,19 +6,39 @@ use Symfony\Component\HttpFoundation\Session;
 $routes = $app['controllers_factory'];
 
 
-$routes->match('/{id}/{patrolEntryID}/{patrol}/', function($id, $patrolEntryID,$patrol) use ($app){
+$routes->match('/{id}/{patrolEntryID}/{patrol}/{lid}/', function($id, $patrolEntryID,$patrol,$lid) use ($app){
 
-	/* Find the volunteer and send it to the template. */
+	/* Find the patrol and it's entries and send it to the template. */
 	$patrolEntry = $app['paris']->getModel('Coastkeeper\PatrolEntry')->find_one($patrolEntryID);
 	$patrol = $app['paris']->getModel('Coastkeeper\Patrol')->find_one($patrol);
+	
+	/* Find the volunteer, section, and loction to send to the template */
 	$volunteer = $app['paris']->getModel('Coastkeeper\Volunteer')->find_one($id);
+	$location = $app['paris']->getModel('Coastkeeper\Location')->find_one($lid);
+	$sid = $patrolEntry->coastkeeper_section_id;
+	$section = $app['paris']->getModel('Coastkeeper\Section')->find_one($sid);
+
+	/* Find all the patrol tallies and their dataEntries */
+	$patrolTallies = $app['paris']->getModel('Coastkeeper\PatrolTally')->find_many();
+	for( $i = 0; $i < count($patrolTallies); ++$i) {
+		if ( $patrolTallies[$i]->coastkeeper_patrol_entry_id - $patrolEntryID == 0){
+			$deid = $patrolTallies[$i]->coastkeeper_datasheet_entry_id;
+			$dataEntries[$i] = $app['paris']->getModel('Coastkeeper\DatasheetEntry')->find_one($deid);
+		}
+	}
 
 
 	/* Get information on a certain patrol */
+	/* Pass all the variables to the template */
 	return $app['twig']->render('admin/volunteers/patrol.twig.html', array(
 		"patrol" => $patrol, 
 		"patrolEntry" => $patrolEntry,
 		"volunteer" => $volunteer,
+		"location" => $location,
+		"section" => $section,
+		"patrolTallies" => $patrolTallies,
+		"dataEntries" => $dataEntries,
+		"patrolEntryID" => $patrolEntryID
 	));
 
 })->assert('id','\d+')
