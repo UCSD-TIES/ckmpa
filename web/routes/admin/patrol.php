30982a3a087765819a640022aeb9b29b901d7e06
Shannon Harris <svjharris@gmail.com>
2013-03-10 15:53:19 -0700

    patrol page now works for multi-section patrols

--- web/routes/admin/patrol.php
+++ web/routes/admin/patrol.php
@@ -9,19 +9,29 @@ $routes = $app['controllers_factory'];
 $routes->match('/{id}/{patrolEntryID}/{patrol}/{lid}/', function($id, $patrolEntryID,$patrol,$lid) use ($app){
 
 	/* Find the patrol and it's entries and send it to the template. */
-	$patrolEntry = $app['paris']->getModel('Coastkeeper\PatrolEntry')->find_one($patrolEntryID);
+
+	$patrolEntries = $app['paris']->getModel('Coastkeeper\PatrolEntry')->find_many();
+	$ourPatrolEntries = array();
+	foreach($patrolEntries as $patrolEntry){
+		if( $patrolEntry->coastkeeper_patrol_id == $patrolEntryID){
+			array_push($ourPatrolEntries, $patrolEntry);
+		}
+	}
 	$patrol = $app['paris']->getModel('Coastkeeper\Patrol')->find_one($patrol);
 	
-	/* Find the volunteer, section, and loction to send to the template */
+	/* Find the volunteer, sections, and loction to send to the template */
 	$volunteer = $app['paris']->getModel('Coastkeeper\Volunteer')->find_one($id);
 	$location = $app['paris']->getModel('Coastkeeper\Location')->find_one($lid);
-	$sid = $patrolEntry->coastkeeper_section_id;
-	$section = $app['paris']->getModel('Coastkeeper\Section')->find_one($sid);
+	$sections = array();
+	foreach($ourPatrolEntries as $patrolEntry){
+		$sid = $patrolEntry->coastkeeper_section_id;
+		array_push( $sections, $app['paris']->getModel('Coastkeeper\Section')->find_one($sid));
+	}
 
 	/* Find all the patrol tallies and their dataEntries */
 	$patrolTallies = $app['paris']->getModel('Coastkeeper\PatrolTally')->find_many();
 	for( $i = 0; $i < count($patrolTallies); ++$i) {
-		if ( $patrolTallies[$i]->coastkeeper_patrol_entry_id - $patrolEntryID == 0){
+		if ( $patrolTallies[$i]->coastkeeper_patrol_entry_id == $patrolEntryID){
 			$deid = $patrolTallies[$i]->coastkeeper_datasheet_entry_id;
 			$dataEntries[$i] = $app['paris']->getModel('Coastkeeper\DatasheetEntry')->find_one($deid);
 		}
@@ -36,10 +46,10 @@ $routes->match('/{id}/{patrolEntryID}/{patrol}/{lid}/', function($id, $patrolEnt
 	/* Pass all the variables to the template */
 	return $app['twig']->render('admin/volunteers/patrol.twig.html', array(
 		"patrol" => $patrol, 
-		"patrolEntry" => $patrolEntry,
+		"patrolEntries" => $ourPatrolEntries,
 		"volunteer" => $volunteer,
 		"location" => $location,
-		"section" => $section,
+		"sections" => $sections,
 		"patrolTallies" => $patrolTallies,
 		"dataEntries" => $dataEntries,
 		"patrolEntryID" => $patrolEntryID,
