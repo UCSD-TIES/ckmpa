4879559b10821a76ec0e31650884817515204d72
David Drabik <djdrabik@gmail.com>
2013-05-14 03:08:43 +0000

    added patrols page

--- web/routes/admin/patrols.php
+++ web/routes/admin/patrols.php
@@ -3,7 +3,6 @@
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 
 /*
@@ -14,56 +13,137 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 $routes = $app['controllers_factory'];
 
 /*
- * Route to display patrols
- *   - If no location ID is specified, displays patrols from all locations
- * 	 - If no section ID is specified, displays patrols from the specific location
- *   - Otherwise, displays patrols from specific location/section
- * Author: David Drabik - djdrabik@gmail.com
+ * Route to display all patrols
  */
-$routes->get('/{location_id}/{section_id}', function($location_id, $section_id) use ($app) {
-
+$routes->get('/', function() use ($app) {
 	$patrols = array();
+	$locations = $app['paris']->getModel('Coastkeeper\Location')->find_many();
 
-	/* Get the location and section the user is requesting */
-	if($location_id == 0) {
-		$location = $app['paris']->getModel('Coastkeeper\Location')->find_many();	
-	} else {
-		$location = $app['paris']->getModel('Coastkeeper\Location')->find_one($location_id);
-		if(!isset($location->id)) { $app->abort(404, "Location with that id does not exist"); }
+	foreach ($locations as $location) {
+		$sections = $location->sections()->find_many();
+
+		foreach ($sections as $section) {
+			$patrol_entries = $section->patrol_entry()->find_many();
+
+			foreach ($patrol_entries as $patrol_entry) {
+				$data = array();
+				$patrol = $patrol_entry->patrol()->find_one();
+
+				$volunteer = $patrol->volunteer()->find_one();
+
+				if ($volunteer) {
+					$data['volunteer'] = $volunteer->first_name . ' ' . $volunteer->last_name;
+				}
+
+				$data['location']  = $location;
+				$data['section']   = $section;
+				$data['date'] = $patrol->date;
+				$data['finished'] = $patrol->finished;
+
+
+				array_push($patrols, $data);
+			}
+		}
 	}
 
-	/* Get the section the user is requesting */
-	if($section_id == 0) {
-		$section = $app['paris']->getModel('Coastkeeper\Section')->find_many();
-	} else {
-		$section = $app['paris']->getModel('Coastkeeper\Section')->find_one($section_id);
-		if(!isset($section->id)) { $app->abort(404, "Section with that id does not exist"); }
-	}
-
-	/* Check whether the location/section combo is valid if both are specified */
-	if((!$location_id && !$section_id ) 						
-		&& !is_array($location) && !is_array($section)			
-		&& $location->id != $section->coastkeeper_location_id) {
-		$app->abort(404, "Section doesn't exist at this MPA");
-	}
 
 
-	if($location_id) {
-		$patrols = $app['paris']->getModel('Coastkeeper\Patrol')
-					->where_equal('coastkeeper_location_id', $location_id)
-					->join('coastkeeper_patrol_entry', "coastkeeper_patrol.id = coastkeeper_patrol_id")
-					->find_many();
-	} else {
-		$patrols = $app['paris']->getModel('Coastkeeper\Patrol')
-					->join('coastkeeper_patrol_entry', "coastkeeper_patrol.id = coastkeeper_patrol_id")
-					->find_many();
-	}
+
 
 	/* Render the html file, passing in the values */
 	return $app['twig']->render('admin/patrols/list.twig.html', array(
 		'patrols' => $patrols,
-		'section' => $section,
-		'location' => $location
+	));
+})->before($admin_login_check)
+  ->bind('admin_patrols_all');
+
+/*
+ * Route to display patrols from a specific location, but all sections
+ * Author: David Drabik - djdrabik@gmail.com
+ */
+$routes->get('/{location_id}/', function($location_id) use ($app) {
+	$patrols = array();
+	$location = $app['paris']->getModel('Coastkeeper\Location')->find_one($location_id);
+	if (!$location) {
+		$app->abort(404, 'Section does not exist');
+	}
+
+	$sections = $location->sections()->find_many();
+
+
+	foreach ($sections as $section) {
+		$patrol_entries = $section->patrol_entry()->find_many();
+
+
+		foreach ($patrol_entries as $patrol_entry) {
+			$data = array();
+			$patrol = $patrol_entry->patrol()->find_one();
+
+			$volunteer = $patrol->volunteer()->find_one();
+
+			if ($volunteer) {
+				$data['volunteer'] = $volunteer->first_name . ' ' . $volunteer->last_name;
+			}
+
+			$data['location']  = $location;
+			$data['section']   = $section;
+			$data['date'] = $patrol->date;
+			$data['finished'] = $patrol->finished;
+
+
+			array_push($patrols, $data);
+		}
+	}
+	
+	/* Render the html file, passing in the values */
+	return $app['twig']->render('admin/patrols/list.twig.html', array(
+		'patrols' => $patrols,
+	));
+})->assert('location_id', '\d+')
+  ->before($admin_login_check)
+  ->bind('admin_patrols_one');
+
+/*
+ * Route to display patrols from the specific location and section
+ * Author: David Drabik - djdrabik@gmail.com
+ */
+$routes->match('/{location_id}/{section_id}/', function($location_id, $section_id) use ($app) {
+
+	$patrols = array();
+	$location = $app['paris']->getModel('Coastkeeper\Location')->find_one($location_id);
+	if (!$location) {
+		$app->abort(404, 'Section does not exist');
+	}
+
+	$section = $location->sections()->find_one($section_id);
+	if (!$section) {
+		$app->abort(404, 'Section does not exist');
+	}
+
+	$patrol_entries = $section->patrol_entry()->find_many();
+
+	foreach ($patrol_entries as $patrol_entry) {
+		$data = array();
+		$patrol = $patrol_entry->patrol()->find_one();
+
+		$volunteer = $patrol->volunteer()->find_one();
+
+		if ($volunteer) {
+			$data['volunteer'] = $volunteer->first_name . ' ' . $volunteer->last_name;
+		}
+
+		$data['location']  = $location;
+		$data['section']   = $section;
+		$data['date'] = $patrol->date;
+		$data['finished'] = $patrol->finished;
+
+
+		array_push($patrols, $data);
+	}
+	
+	/* Render the html file, passing in the values */
+	return $app['twig']->render('admin/patrols/list.twig.html', array(
+		'patrols' => $patrols,
 	));
 
 })->value('location_id', 0)
