eead95a363f9a9c6ae6bec3f96d8bcd58cff6db9
Jungyoon Chung <JungyoonChung@Jungyoon-Chungs-MacBook-Pro.local>
2013-05-30 20:03:12 -0700

    updated graphs.js

--- web/js/graphs.js
+++ web/js/graphs.js
@@ -19,3 +19,23 @@ var data = {
 			}
 			var ctx = document.getElementById("myChart").getContext("2d");			
 			var myNewChart = new Chart(ctx).Line(data,null);	
+
+
+var data2 = {
+	labels : ["January","February","March","April","May","June","July"],
+	datasets : [
+		{
+			fillColor : "rgba(220,220,220,0.5)",
+			strokeColor : "rgba(220,220,220,1)",
+			data : [65,59,90,81,56,55,40]
+		},
+		{
+			fillColor : "rgba(151,187,205,0.5)",
+			strokeColor : "rgba(151,187,205,1)",
+			data : [28,48,40,19,96,27,100]
+		}
+	]
+}
+
+			var ctx2 = document.getElementById("myChart2").getContext("2d");			
+			var myNewChart = new Chart(ctx2).Bar(data,null);	
