1f6d2aeb83e6a42da1480223a4f32b54e5525540
David Drabik <djdrabik@gmail.com>
2013-05-31 01:24:45 +0000

    added field listing

---
+++ web/routes/admin/fields.php
@@ -0,0 +1,218 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session;
+
+/*
+ *  All URLS will be prefixed with /admin/fields/ automatically
+ *  by admin.php
+ */
+
+$routes = $app['controllers_factory'];
+
+/* NOTE: $admin_login_check is defined in admin.php */
+
+/*
+	MPA (Locations) Management
+ */
+$routes->get('/', function() use ($app){
+
+        $data = array();
+
+        /* Get a list of mpas */
+        $datasheets = $app['paris']->getModel('Coastkeeper\Datasheet')->find_many();
+
+        foreach ($datasheets as $sheet) {
+            $categories = $sheet->categories()->find_many();
+
+            foreach ($categories as $category) {
+                $entries = $category->entries()->find_many();
+
+                foreach ($entries as $entry) {
+                    $fields = array();
+                    $fields["category"] = $category;
+                    $fields["entry"] = $entry;
+
+                    array_push($data, $fields);
+                }
+
+            }
+
+        }
+
+        /* Display the list of fields */
+        return $app['twig']->render('admin/fields/list.twig.html', array(
+            'data'     => $data
+        ));
+        
+
+})->before($admin_login_check)->bind('admin_fields');
+
+// /*
+//  * Display a specific location's sections
+//  */
+// $routes->get('/{id}/', function($id) use ($app){
+
+//     /* Find the sections with the same coastkeeper_location_id and send it to the template. */
+//     $location = $app['paris']->getModel('Coastkeeper\Location')->find_one($id);
+
+//     $section = $app['paris']->getModel('Coastkeeper\Section')
+//         ->where_equal('coastkeeper_location_id', $id)
+//         ->find_many();
+
+//     /* Get information on a certain location */
+//     return $app['twig']->render('admin/locations/view.twig.html', array(
+//         "section" => $section,
+//         "location" => $location
+//     )); 
+
+// })->assert('id','\d+')
+//   ->before($admin_login_check)
+//   ->bind('admin_locations_view');
+
+// $routes->match('/create/', function(Request $request) use ($app){
+
+// 	/* Errors */
+// 	$errors = array();
+
+// 	if('POST' == $request->getMethod())
+// 	{
+
+// 		$location_name = $request->get('location_name');
+
+// 		/* Validity Checks. */
+
+//             /* Location name cannot be blank */
+//             if( empty($location_name) )
+//             {
+//                     $errors['location_name'] = "Please enter an name for the new location";
+//             }
+
+//         /* Name must be unique */
+//         if( $app['paris']->getModel('Coastkeeper\Location')
+//                                       ->where_equal('name', $location_name)
+//                                       ->find_one()) {
+//           $errors['location_name'] = "There is already a location with that name";
+//         }
+
+// 		/* If everything is ok, create the new location */
+// 		if(count($errors) <= 0)
+// 		{
+// 			$location = $app['paris']->getModel('Coastkeeper\Location')->create();
+// 			$location->name = $location_name;
+//                         $location->coastkeeper_datasheet_id = 1;
+
+// 			$location->save();
+
+//             $locations = $app['paris']->getModel('Coastkeeper\Location')->find_many();
+
+// 			return $app->redirect($app['url_generator']->generate('admin_locations'));
+// 		}
+
+// 	}
+
+// 	/* Render the create form. */
+// 	return $app['twig']->render('admin/locations/create.twig.html', array(
+// 		"errors" => $errors,
+// 	));
+
+// })->before($admin_login_check)->bind('admin_locations_create');
+
+// $routes->match( '/{id}/delete/', function( REQUEST $request, $id ) use ( $app ) {
+
+//     /* get the location */
+//     $location = $app['paris']->getModel('Coastkeeper\Location')->find_one($id);
+
+//     if( 'POST' == $request->getMethod() && $location ) {
+
+//         // check for delete approval
+//         if( $request->get('approve_delete')) {
+
+//             /* Delete the location */
+//             $location->delete();
+
+//             /* Return to the locations list */
+//             return $app->redirect( $app['url_generator']->generate('admin_locations'));
+//         }
+
+//     }
+
+//     /* display delete confirmation form */
+//     return $app['twig']->render('admin/locations/delete.twig.html', array(
+//         "location" => $location
+//     ));
+
+// })->assert('id','\d+')
+//     ->before( $admin_login_check )
+//     ->bind('admin_locations_delete');
+
+// $routes->match( '/{id}/edit/', function( REQUEST $request, $id ) use ( $app ) {
+
+//     /* Array for errors */
+//     $errors = array();
+
+//     /* get the location */
+//     $location = $app['paris']->getModel('Coastkeeper\Location')->find_one($id);
+
+//     if( 'POST' == $request->getMethod() && $location ) {
+
+//         /* Get the user input */
+//         $location_name = $request->get('location_name');
+
+//         /*
+//          * Validity checks
+//          */
+
+//         /* Location name cannot be blank */
+//         if( empty($location_name) )
+//         {
+//             $errors['location_name'] = "Please enter an name for the location";
+//         }
+
+//         /* Name must consist of letters and numbers */
+//         if( !empty($location_name) && !ctype_alnum($location_name) ) {
+//           $errors['location_name'] = "Please use only letters and/or numbers for the location's name";
+//         }
+
+//         /* Name must be unique */
+//         if( $app['paris']->getModel('Coastkeeper\Location')
+//                                       ->where_equal('name', $location_name)
+//                                       ->find_one()) {
+//             $errors['location_name'] = "There is already a location with that name";
+//         }
+
+//         /* If everything is ok, update the location */
+//         if(count($errors) <= 0)
+//         {
+//             $location->name = $location_name;
+
+//             /* Update the location */
+//             $location->save();
+
+
+//             $locations = $app['paris']->getModel('Coastkeeper\Location')->find_many();
+
+//             return $app->redirect($app['url_generator']->generate('admin_locations'));
+//         }
+//     }
+
+//     /* Render the edit locations form */
+//     return $app['twig']->render('admin/locations/edit.twig.html', array(
+//         "errors"   => $errors,
+//         "location" => $location
+//     ));
+
+// })->assert('id', '\d+')
+//  ->before($admin_login_check)
+//  ->bind('admin_locations_edit');
+
+return $routes;
+
+function array_pshift(&$array) {
+    $keys = array_keys($array);
+    $key = array_shift($keys);
+    $element = $array[$key];
+    unset($array[$key]);
+    return $element;
+} 
