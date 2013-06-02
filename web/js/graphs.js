99c993a33bd44e63ae500404f5943b99b215d3b4
David Drabik <djdrabik@gmail.com>
2013-06-02 08:26:58 +0000

    progress with graphs

--- web/js/graphs.js
+++ web/js/graphs.js
@@ -1,41 +1,64 @@
-var data = {
-  				labels : ["January","February","March","April","May","June","July"],
+(function() {
+	$('#graphs-form').on('submit', function(e) {
+		var graphLabels = [],
+			graphData = [],
+			$this = $(this);
+			
+		e.preventDefault();
+
+		console.log($this.attr('action'));
+		$.ajax({
+			type: "GET",
+			url: $this.attr('action'),
+			data: $this.serialize(),
+			dataType: "json"
+		}).done(function(data) {
+			console.log(data);
+			$.each(data, function(i, obj) {
+				graphLabels.push(i);
+			    graphData.push(obj);
+			});
+
+			console.log(graphData);
+			displayGraph(graphData, graphLabels);
+		});
+	});
+
+	var displayGraph = function(gData, gLabels) {
+		var data = {
+				labels : gLabels,
 				datasets : [
-					{
-						fillColor : "rgba(220,220,220,0.5)",
-						strokeColor : "rgba(220,220,220,1)",
-						pointColor : "rgba(220,220,220,1)",
-						pointStrokeColor : "#fff",
-						data : [65,59,90,81,56,55,40]
-					},
-					{
-						fillColor : "rgba(151,187,205,0.5)",
-						strokeColor : "rgba(151,187,205,1)",
-						pointColor : "rgba(151,187,205,1)",
-						pointStrokeColor : "#fff",
-						data : [28,48,40,19,96,27,100]
-					}
-				]
-			}
-			var ctx = document.getElementById("myChart").getContext("2d");			
-			var myNewChart = new Chart(ctx).Line(data,null);	
-
-
-var data2 = {
-	labels : ["January","February","March","April","May","June","July"],
-	datasets : [
-		{
-			fillColor : "rgba(220,220,220,0.5)",
-			strokeColor : "rgba(220,220,220,1)",
-			data : [65,59,90,81,56,55,40]
-		},
-		{
-			fillColor : "rgba(151,187,205,0.5)",
-			strokeColor : "rgba(151,187,205,1)",
-			data : [28,48,40,19,96,27,100]
+				{
+					fillColor : "rgba(220,220,220,0.5)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					data : gData
+				},
+			]
 		}
-	]
-}
+		var ctx = document.getElementById("myChart").getContext("2d");			
+		var myNewChart = new Chart(ctx).Bar(data,null);	
+	}
+	
 
-			var ctx2 = document.getElementById("myChart2").getContext("2d");			
-			var myNewChart = new Chart(ctx2).Bar(data,null);	
+
+	// var data2 = {
+	// 	labels : ["January","February","March","April","May","June","July"],
+	// 	datasets : [
+	// 		{
+	// 			fillColor : "rgba(220,220,220,0.5)",
+	// 			strokeColor : "rgba(220,220,220,1)",
+	// 			data : [65,59,90,81,56,55,40]
+	// 		},
+	// 		{
+	// 			fillColor : "rgba(151,187,205,0.5)",
+	// 			strokeColor : "rgba(151,187,205,1)",
+	// 			data : [28,48,40,19,96,27,100]
+	// 		}
+	// 	]
+	// }
+
+	// 			var ctx2 = document.getElementById("myChart2").getContext("2d");			
+	// 			var myNewChart = new Chart(ctx2).Bar(data,null);	
+})();
--- web/routes/admin/graphs.php
+++ web/routes/admin/graphs.php
@@ -16,43 +16,69 @@ $routes = $app['controllers_factory'];
 /*
 	MPA (Locations) Management
  */
-$routes->get('/', function() use ($app){
+$routes->get('/', function(Request $request) use ($app){
 
-	$patrols = array();
-	$locations = $app['paris']->getModel('Coastkeeper\Location')->find_many();
-	
-	foreach ($locations as $location) {
-		$sections = $location->sections()->find_many();
-
-		foreach ($sections as $section) {
-			$patrol_entries = $section->patrol_entry()->find_many();
-
-			foreach ($patrol_entries as $patrol_entry) {
-				$data = array();
-				$patrol = $patrol_entry->patrol()->find_one();
-
-				$volunteer = $patrol->volunteer()->find_one();
-
-				if ($volunteer) {
-					$data['volunteer'] = $volunteer->first_name . ' ' . $volunteer->last_name;
-				}
-
-				$data['location']  = $location;
-				$data['section']   = $section;
-				$data['date'] = $patrol->date;
-				$data['finished'] = $patrol->finished;
+	$datasheets = $app['paris']->getModel('Coastkeeper\Datasheet')->find_many();
 
 
-				array_push($patrols, $data);
-			}
-		}
-	}
-        
     /* Render the html file, passing in the values */
 	return $app['twig']->render('admin/graphs/create.twig.html', array(
-		'graphs' => $patrols,
+		'datasheets' => $datasheets
 	));
         
 })->before($admin_login_check)->bind('admin_graphs');
+
+$routes->get('/data', function(Request $request) use ($app) {
+
+	$startYear = $app['request']->get('startYear');
+	$startMonth = $app['request']->get('startMonth');
+	$startDay = $app['request']->get('startDay');
+
+	$endYear = $app['request']->get('endYear');
+	$endMonth = $app['request']->get('endMonth');
+	$endDay = $app['request']->get('endDay');
+
+	$datasheet_id = $app['request']->get('datasheet');
+
+	$datasheet = $app['paris']->getModel('Coastkeeper\Datasheet')->find_one($datasheet_id);
+
+	$locations = $datasheet->locations()->find_many();
+
+	$data = array();
+	foreach ($locations as $location) {
+		$sections = $location->sections()->find_many();
+
+		foreach($sections as $section) {
+			$patrol_entries = $section->patrol_entry()->find_many();
+
+			/* For each patrol entry, fill out a row */
+			foreach($patrol_entries as $patrol_entry) {
+
+				/* Get the parent patrol. */
+				$patrol = $patrol_entry->patrol()->find_one();
+
+				/* Now get all the tallies for this patrol... */
+				$tallies = $patrol_entry->patrol_tallies()->find_many();
+
+				foreach($tallies as $tally) {
+					if ($tally->tally === "1") {
+						if (array_key_exists($tally->coastkeeper_datasheet_entry_id, $data)) {
+							$data[$tally->coastkeeper_datasheet_entry_id] += 1;
+						} else {
+							$data[$tally->coastkeeper_datasheet_entry_id] = 1;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// echo '<pre>';
+	// var_dump($data);
+	// echo '</pre>';
+
+	return $app->json($data, 201);
+})->before($admin_login_check)->bind('admin_graphs_data');
+
 
 return $routes;
