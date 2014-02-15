25441bc09adbf987cc9055a5f6c6069c3a424d87
jcervantes <jcervant@ucsd.edu>
2014-02-14 16:55:41 -0800

    cleaned up admin homepage

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -1,6 +1,13 @@
 @extends('admin/base')
 @section('content')
-	<h1>Dashboard</h1>
+	<h1>My Admin Panel</h1>
 
-	<p>Welcome to your Dashboard. You rock!</p>
+	<h4 style ="text-align:center">
+Welcome to your Dashboard. Coastkeeper administrators can manage the mobile web application from this page and view the data collected from the volunteers. You can manage and add new MPAS and locations, edit observations on the data sheet, update volunteers, and view data from patrol entries with graphs or export to Excel.
+	</h4>
+	<div class="col-xs-offset-4">
+	<img src="assets/Coastkeepersd.png" alt = "some picture">
+	
+    </img>
+    </div>
 @stop
