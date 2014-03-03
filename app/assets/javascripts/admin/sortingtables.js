3279e24a47913a0acf945f9d3f30c5170490ac8f
jcervantes <jcervant@ucsd.edu>
2014-03-02 17:30:14 -0800

    made sort table bootstrap3

--- app/assets/javascripts/admin/sortingtables.js
+++ app/assets/javascripts/admin/sortingtables.js
@@ -1,11 +1,14 @@
  $(document).ready(function() {
  	var table_default = {
-    
+        "aoColumnDefs": [
+        { "bSortable": false, "aTargets": [ 3 ] },
+    ],
     "iDisplayLength": 15,
     "aLengthMenu": [
         [15, 30, 50, -1],
         [15, 30, 50, "All"]
     ],
+    
     "fnPreDrawCallback": function (oSettings, json) {
         $('.dataTables_filter input').addClass('form-control');
         $('.dataTables_length select').addClass('form-control');
