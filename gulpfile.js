9559509749dec64915b4fd1df1241bc3679c5649
Alex Chen <elkhouri1@gmail.com>
2014-04-25 02:21:26 -0700

    Gulp build scripts for mobile app

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,61 @@
+/* jshint node:true */
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var notify = require('gulp-notify');
-var sass = require('gulp-ruby-sass');
-var autoprefix = require('gulp-autoprefixer');
-var minifyCSS = require('gulp-minify-css')
-var coffee = require('gulp-coffee');
-var ls = require('gulp-livescript');
+var tar = require('gulp-tar');
+var gzip = require('gulp-gzip');
+var es = require('event-stream');
 var livereload = require('gulp-livereload');
-var lr = require('tiny-lr')
+var lr = require('tiny-lr');
 var server = lr();
 server.listen(35729);
 
-// Where do you store your Sass files?
-var sassDir = 'app/assets/sass';
- 
-// Which directory should Sass compile to?
-var targetCSSDir = 'public/css';
- 
-// Where do you store your CoffeeScript files?
-var coffeeDir = 'app/assets/coffee';
-var lsDir = 'app/assets/livescript';
-
-// Which directory should CoffeeScript compile to?
-var targetJSDir = 'public/js';
- 
 var htmlDir = 'public/';
-
-// Compile Sass, autoprefix CSS3,
-// and save to target CSS directory
-gulp.task('css', function () {
-    return gulp.src(sassDir + '/main.sass')
-        .pipe(sass({ style: 'compressed' }).on('error', gutil.log))
-        .pipe(autoprefix('last 10 version'))
-        .pipe(gulp.dest(targetCSSDir))
-        .pipe(notify('CSS minified'))
-});
- 
-// // Handle CoffeeScript compilation
-// gulp.task('js', function () {
-//     return gulp.src(coffeeDir + '/**/*.coffee')
-//         .pipe(coffee().on('error', gutil.log))
-//         .pipe(gulp.dest(targetJSDir))
-// });
-
-gulp.task('js', function () {
-    return gulp.src(lsDir + '/**/*.ls')
-        .pipe(ls({bare: true}).on('error', gutil.log))
-        .pipe(gulp.dest(targetJSDir))
-});
+var buildDir = 'mobile_build/';
 
 gulp.task('html', function () {
-    return gulp.src(htmlDir + '/**/*.html')
-        .pipe(livereload(server));
-})
- 
- 
-// Keep an eye on Sass, Coffee, and PHP files for changes...
-gulp.task('watch', function () {
-    //gulp.watch(sassDir + '/**/*.sass', ['css']);
-    //gulp.watch(coffeeDir + '/**/*.coffee', ['js']);
-    gulp.watch(lsDir + '/**/*.ls', ['js']);
-    gulp.watch(htmlDir + '/**/*.html', ['html']);
+  return gulp.src(htmlDir + '/**/*.html')
+    .pipe(livereload(server));
 });
- 
-// What tasks does running gulp trigger?
-gulp.task('default', ['js', 'watch']);
+
+gulp.task('watch', function () {
+  gulp.watch(htmlDir + '/**/*.html', ['html']);
+});
+
+function appStream () {
+  return gulp.src(['js/*.js',
+                   'css/*.css',
+                   'partials/*.html',
+                   'templates/*.html',
+                   'img/*.*',
+                   'index.html',
+                   '!lib/**'], {
+    cwd: 'public/**'
+  });
+}
+
+function libStream () {
+  return gulp.src(['lib/angular/angular.min.js',
+                   'lib/angular-animate/angular-animate.min.js',
+                   'lib/angular-local-storage/angular-local-storage.min.js',
+                   'lib/angular-resource/angular-resource.min.js',
+                   'lib/angular-sanitize/angular-sanitize.min.js',
+                   'lib/angular-ui-router/release/angular-ui-router.min.js',
+                   'lib/ionic/js/ionic.min.js',
+                   'lib/ionic/js/ionic-angular.min.js',
+                   'lib/ionic/css/*.css',
+                   'lib/ionic/fonts/*.*',
+                   'lib/lodash/dist/lodash.min.js'], {
+    cwd: 'public/**'
+  });
+}
+
+gulp.task('dist', function(){
+  return es.merge(appStream(), libStream())
+  .pipe(tar('app.tar'))
+  .pipe(gzip())
+  .pipe(gulp.dest(''));
+});
+
+gulp.task('default', ['watch']);
+gulp.task('build', ['build-app', 'build-lib']);
