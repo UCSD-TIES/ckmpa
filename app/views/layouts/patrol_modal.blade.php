02aff0c4a5404979ad9a8209606a2b4f535f2452
Alex Chen <elkhouri1@gmail.com>
2014-05-25 20:41:31 -0700

    Much improved tabbed patrol details modal that doesn't scroll forever

--- app/views/layouts/patrol_modal.blade.php
+++ app/views/layouts/patrol_modal.blade.php
@@ -5,36 +5,47 @@
   </h4>
 </div>
 <div class="modal-body">
-  <ul class="list-group">
+  <ul class="nav nav-tabs nav-justified" id="myTab">
     @foreach($categories as $category_name => $category)
-    <li class="list-group-item list-group-item-info">
-      <h4 class="list-group-item-heading">{{ $category_name }}</h4>
+    <li><a href="#{{str_replace(' ', '_', $category_name)}}" data-toggle="tab">{{$category_name}}</a>
     </li>
-    @foreach($category as $field_name => $field)
-      @if(is_array($field))
-        <li class="list-group-item">{{ $field_name }}</li>
-          @foreach($field as $subcategory_name => $sub)
-          <li class="list-group-item">
-            {{ $subcategory_name }}
-            <span class="badge">{{ $sub }}</span>
-          </li>
-        @endforeach
-      @else
-        <li class="list-group-item">
-          {{ $field_name }}
-          <span class="badge">{{ $field }}</span>
-        </li>
-      @endif
-
     @endforeach
-  @endforeach
   </ul>
 
-  <h3>Comments</h3>
-  <p>
-    {{ $patrol->comments or "None" }}
-  </p>
-</div>
-<div class="modal-footer">
-  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+  <div class="tab-content">
+    @foreach($categories as $category_name => $category)
+      <div class="tab-pane" id="{{str_replace(' ', '_', $category_name)}}">
+        <div class='list-group'>
+        @foreach($category as $field_name => $field)
+
+          @if(is_array($field))
+          <div class="list-group-item list-group-item-info">{{ $field_name }}</div>
+            @foreach($field as $subcategory_name => $sub)
+            <li class="list-group-item">
+              {{ $subcategory_name }}
+              <span class="badge">{{ $sub }}</span>
+            </li>
+            @endforeach
+
+          @else
+            <li class="list-group-item">
+              {{ $field_name }}
+              <span class="badge">{{ $field }}</span>
+            </li>
+          @endif
+
+        @endforeach
+        </div>
+      </div>
+    @endforeach
+
+    <h3>Comments</h3>
+    <p>
+      {{ $patrol->comments or "None" }}
+    </p>
+  </div>
+
+  <div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+  </div>
 </div>
